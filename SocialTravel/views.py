d3558e18a02d9840b5e26f383e0a1c36be956029
Juan Norry <juansnorry@gmail.com>
2023-03-10 18:34:47 -0300

    Formuralios de carga y busqueda

--- SocialTravel/views.py
+++ SocialTravel/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from SocialTravel.models import Post
+from SocialTravel.forms import PostForm
 
 def index(request):
     return render(request, "SocialTravel/index.html")
 
+
 def mostra_otro_template(request):
-
     posts = Post.objects.all()
-
-
-
     return render(request, "SocialTravel/otro_template.html",{"posts": posts})
+
+
+def mostrar_posts(request):
+    context = {
+        "form": PostForm(),
+        "posts": Post.objects.all()
+    }
+
+    return render(request, "SocialTravel/admin_post.html", context) 
+
+
+def agregar_post(request):
+    post_form = PostForm(request.POST)
+    post_form.save()
+    context = {
+        "form": PostForm(),
+        "posts": Post.objects.all()
+    }
+
+    return render(request, "SocialTravel/admin_post.html", context)
+
+
+def buscar_post(request):
+    criterio = request.GET.get("criterio")
+    context = {
+        "posts": Post.objects.filter(carousel_caption_title__icontains=criterio).all(),
+        }
+    
+    return render(request, "SocialTravel/admin_post.html", context)
--- seed_data.py
+++ seed_data.py
@@ -4,22 +4,26 @@ Post(carousel_caption_title="Un Carousel Title",
      carousel_caption_description="Un Carousel descript",
      heading="MI viaje",
      description="una description",
+     un_campo=""
      ).save()
 
 Post(carousel_caption_title="Un Carousel Title 1",
      carousel_caption_description="Un Carousel descript 1",
      heading="MI viaje 1",
      description="una description 1",
+     un_campo=""
      ).save()
 
 Post(carousel_caption_title="Un Carousel Title 2",
      carousel_caption_description="Un Carousel descript 2",
      heading="MI viaje 2",
      description="una description 2",
+     un_campo=""
      ).save()
 
 Post(carousel_caption_title="Un Carousel Title 3",
      carousel_caption_description="Un Carousel descript 3",
      heading="MI viaje 3",
      description="una description 3",
+     un_campo=""
      ).save()
